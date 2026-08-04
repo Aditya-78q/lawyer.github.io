5b7bc0b8f2fe887555bf8a6f56b0cc2bb9a93c72
Aditya-78q <132346812+Aditya-78q@users.noreply.github.com>
2026-08-04 11:53:25 +0530

    fixed

--- js/main.js
+++ js/main.js
@@ -301,9 +301,4 @@
 
 
 }());
-<script>
-  if (window.location.pathname.endsWith('.html')) {
-    const cleanUrl = window.location.pathname.replace('.html', '');
-    window.location.replace(cleanUrl);
-  }
-</script>
+
