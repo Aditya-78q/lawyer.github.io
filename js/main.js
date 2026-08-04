22bb55a6c7e2024cd01649b7e8d1d67162dfcb97
Aditya-78q <132346812+Aditya-78q@users.noreply.github.com>
2026-08-04 11:50:53 +0530

    hide pages
    Add script to redirect .html URLs to clean URLs.

--- js/main.js
+++ js/main.js
@@ -301,3 +301,9 @@
 
 
 }());
+<script>
+  if (window.location.pathname.endsWith('.html')) {
+    const cleanUrl = window.location.pathname.replace('.html', '');
+    window.location.replace(cleanUrl);
+  }
+</script>
